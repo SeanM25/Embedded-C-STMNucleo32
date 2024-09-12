8777b8b06b94560b1d6fdfb8d566fad60bd13de5
Jake Lee <jlee211@ucsc.edu>
2024-09-12 16:05:41 -0700

    added template Calculator

---
+++ Lab02/templates/Calculator_template.c
@@ -0,0 +1,92 @@
+/*
+ *
+ */
+// Standard libraries.
+#include <stdio.h>
+#include <math.h>
+
+
+// **** Declare function prototypes here. ****
+double Add(double operand1, double operand2);
+double Multiply(double operand1, double operand2);
+double AbsoluteValue(double operand);
+double FahrenheitToCelsius(double operand);
+double Tangent(double operand);
+// Add more prototypes below.
+
+
+void CalculatorRun(void)
+{
+    printf(
+        "\n\nWelcome to NAME's calculator program! Compiled at %s %s\n",
+        __DATE__,
+        __TIME__
+    );
+    // Replace this while(1) with your main code loop.
+
+    while(1); 
+}
+
+
+
+/*******************************************************************************
+ * Define the Add function here.
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Define the Subtract function here.
+ ******************************************************************************/
+double Subtract(double operand1, double operand2)
+{
+
+}
+
+/*******************************************************************************
+ * Define the Multiply function here.
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Define the Divide function here.
+ ******************************************************************************/
+double Divide(double operand1, double operand2)
+{
+	
+}
+
+/*******************************************************************************
+ * Define the Absolute Value function here.
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Define the Fahrenheit to Celsius function here.
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Define the Celsius to Fahrenheit function here.
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Define the Average function here.
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Define the Tangent function that takes input in degrees.
+ ******************************************************************************/
+double Tangent
+
+/*******************************************************************************
+ * Define the Round function here.
+ * In order to receive the extra credit your calculator MUST ALSO CORRECTLY
+ * utilize this function.
+ ******************************************************************************/
+double Round(double operand)
+{
+
+}
+
