e8a2d9d1959c23f08682a57d4668b2223b16d57a
Jake Lee <jlee211@ucsc.edu>
2024-09-16 20:25:32 -0700

    updated part2_template.c to spam output somewhat

--- Lab01/templates/part2_template.c
+++ Lab01/templates/part2_template.c
@@ -23,17 +23,19 @@
  */
 int main(void) {
     BOARD_Init();
-    /***************************************************************************
-     * Your code goes in between this comment and the following one with       *
-     * asterisks.                                                              *
-     **************************************************************************/
+    while(1) {
+        /***************************************************************************
+         * Your code goes in between this comment and the following one with       *
+         * asterisks.                                                              *
+         **************************************************************************/
 
-    /***************************************************************************
-     * Your code goes in between this comment and the preceding one with       *
-     * asterisks.                                                              *
-     **************************************************************************/
-
+        /***************************************************************************
+         * Your code goes in between this comment and the preceding one with       *
+         * asterisks.                                                              *
+         **************************************************************************/
+        HAL_Delay(3333);
+    }
     // Returning from main() is bad form in embedded environments.
     // So we sit and spin.
-    while(1);
+    while(1<2);
 }
