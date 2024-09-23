fd51b084d96b0a2bb9834878b38c462c5c6fb460
Jake Lee <jlee211@ucsc.edu>
2024-09-22 23:53:01 -0700

    added template for Leds.c; could use input on what should be expected of students

---
+++ Lab07/templates/Leds_template.c
@@ -0,0 +1,90 @@
+/**
+ * @file    Leds.c
+ * @author
+ * @date
+ *
+ * This library provides an interface for controlling the LEDs on the UCSC
+ * Nucleo I/O Shield development board.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <BOARD.h>
+#include <Leds.h>
+#ifdef STM32F4
+#include "stm32f4xx_hal.h"
+#endif  /*  STM32F4 */
+
+/**
+ * LEDs_Init() Initializes the LED bar by doing three things:
+ *      1) Enables usage of the GPIO clocks for needed ports.
+ *      2) Use the appropriate SFRs to set each LED pin to "output" mode.
+ *      3) Use the appropriate SFRs to set each LED pin's output value to 0 (low
+ *         voltage). 
+ * After calling LEDs_Init(), the other functions in this file can be used to 
+ * manipulate the LED bar.
+ */
+void LEDs_Init(void) {
+    // Enable GPIO clocks for ports C and B.
+#ifdef STM32F4
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    //init GOIO output pins for leds
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+#endif  /*  STM32F4 */
+    LEDs_Set(0x0); // reset leds
+}
+
+/**
+ * LEDs_Set() controls the output on the LED bar.  Input values are 8-bit 
+ * patterns, where each bit describes the desired state of the corresponding 
+ * LED. So, for example, LEDs_Set(0x80) should  leave the first LED on, and the
+ * last 7 LEDs off. LEDs_Set(0x1F) should turn off the first three LEDs and turn
+ * on the remaining five LEDs. 
+ *
+ * @param newPattern:  An 8-bit value describing the desired output on the LED
+ * bar. 
+ *
+ * LEDs_Set should not change any LED pins to inputs.
+ */
+void LEDs_Set(char newPattern) {
+#ifdef STM32F4
+  /* Your code goes between here and the comment below: */
+
+
+  /* Your code goes between here and the comment above: */
+#endif  /*  STM32F4 */
+}
+
+/**
+ * LEDs_Get() returns the current state of the LED bar.  Return values are 8-bit
+ * patterns, where each bit describes the current state of the corresponding
+ * LED. So, for example, if the LED bar's LED's are
+ *
+ * [ON OFF ON OFF   OFF ON OFF ON],
+ *
+ * LEDs_Get() should return 0xA5.
+ * 
+ * @return char:  An 8-bit value describing the current output on the LED bar.  
+ * 
+ * LEDs_Get() should not change the state of the LEDs, or any SFRs.
+ */
+char LEDs_Get(void) {
+    char led_state = 0x00;
+#ifdef STM32F4
+  /* Your code goes between here and the comment below: */
+
+
+  /* Your code goes between here and the comment above: */
+#endif  /*  STM32F4 */
+    return led_state;
+}
+
