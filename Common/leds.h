d90ea672b6f7f807f7740bbded7d3d43e4615b3e
Jake Lee <jlee211@ucsc.edu>
2024-09-22 17:31:10 -0700

    made Leds.h a standalone header file; still needs some macros for LED pin assignments

--- Common/leds.h
+++ Common/leds.h
@@ -1,26 +1,53 @@
-/*
- * File:   leds.h
- * Author: Adam Korycki
+/**
+ * @file    Leds.h
+ * @author  nubby (jlee211@ucsc.edu)
+ * @date    22 Sep 2024
+ * @brief   File containing LED, Nucleo board-focused function prototypes.
  *
- * Created on November 15, 2023
+ * This library provides an interface for controlling the LEDs on the UCSC
+ * Nucleo I/O Shield development board.
  */
+
 #ifndef LEDS_H
 #define LEDS_H
 
 /**
- * @function LEDS_Init(void)
- * @param None
- * @return None
- * @brief Initializes GPIO outputs for the eight leds on the IO shield
- * @author Adam Korycki, 2023.11.15 */
-extern void LEDS_Init(void);
+ * LEDs_init() initializes the LED bar by doing two things:
+ *      1) Use the appropriate SFRs to set each LED pin to "output" mode.
+ *      2) Use the appropriate SFRs to set each LED pin's output value to 0 (low
+ *         voltage). 
+ * After calling LEDs_init(), the other functions in this file can be used to 
+ * manipulate the LED bar.
+ */
+void LEDs_init(void);
 
 /**
- * @function set_leds(uint8_t leds)
- * @param leds - byte where each bit represents the state of the leds (lsb -> LED1)
- * @return None
- * @brief sets leds according to byte parameter
- * @author Adam Korycki, 2023.11.15 */
-extern void set_leds(uint8_t leds);
+ * LEDs_set() controls the output on the LED bar.  Input values are 8-bit 
+ * patterns, where each bit describes the desired state of the corresponding 
+ * LED. So, for example, LEDs_set(0x80) should  leave the first LED on, and the
+ * last 7 LEDs off. LEDs_set(0x1F) should turn off the first three LEDs and turn
+ * on the remaining five LEDs. 
+ *
+ * @param newPattern:  An 8-bit value describing the desired output on the LED
+ * bar. 
+ *
+ * LEDs_set should not change any LED pins to inputs.
+ */
+void LEDs_set(char newPattern);
 
-#endif
+/**
+ * LEDs_get() returns the current state of the LED bar.  Return values are 8-bit
+ * patterns, where each bit describes the current state of the corresponding
+ * LED. So, for example, if the LED bar's LED's are
+ *
+ * [ON OFF ON OFF   OFF ON OFF ON],
+ *
+ * LEDs_get() should return 0xA5.
+ * 
+ * @return char:  An 8-bit value describing the current output on the LED bar.  
+ * 
+ * LEDs_get() should not change the state of the LEDs, or any SFRs.
+ */
+char LEDs_get(void);
+
+#endif // LEDS_H
