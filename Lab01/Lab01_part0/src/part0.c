eaecb142b085f78ce0de3b7e61841411314fdbae
Sean Manger <smanger@ucsc.edu>
2024-09-28 21:39:28 -0700

    “Committed Lab01 part0 files! Mention Driver Issue VSC to Steve

---
+++ Lab01/Lab01_part0/src/part0.c
@@ -0,0 +1,25 @@
+/**
+ * Every file should start with a header like this.
+ *
+ * @file part0.c
+ * @author Sean Manger (smanger@ucsc.edu)
+ *  *
+ * @date 28 Sept 2024
+ */
+// Standard libraries.
+#include <stdio.h>
+#include <stdlib.h>
+// Course libraries.
+#include <BOARD.h>
+/**
+ * Hello Nucleo!
+ */
+int main(void) {
+ BOARD_Init();
+ while(1) // In embedded systems, main should never exit.
+ {
+ printf("Hello World!\r\n");
+ HAL_Delay(333); // Add a delay, in ms, before the next iteration.
+ }
+ BOARD_End(); // Handles cleanup of the system.
+}
