afdeb286cb3ef26e8a26b69370722be7e7da9301
Sean Manger <smanger@ucsc.edu>
2024-09-29 20:29:18 -0700

    Correctly Formatted Using Alt+Shift+F

--- Lab01/Lab01_part1/src/part1.c
+++ Lab01/Lab01_part1/src/part1.c
@@ -1,9 +1,9 @@
 /**
- * A brief journey through the debugging and styling of C code.  
+ * A brief journey through the debugging and styling of C code.
  *
  * @file    part1.c
  * @author  Sean Manger (smanger@ucsc.edu)
- * 
+ *
  * @date    September 29, 2024
  */
 // **** Include libraries up here. ****
@@ -14,10 +14,9 @@
 // Course libraries.
 #include <BOARD.h>
 
-
 /**
  * main()
- * 
+ *
  * Put some information about this function here.
  *
  * @return (int) Describe what is returned by a function here. In this case,
@@ -27,30 +26,34 @@
  *                     can skip the "@return" portion of this description, as
  *                     well as omit the "return" statement on the last line
  *                     of this function.
- *               
+ *
  *               NOTE: You can also omit the title of "main()" above since there
  *                     can only be one "main()" function per program.
  */
-int main(void) {
+int main(void)
+{
     BOARD_Init();
     unsigned char vals[] = {rand(), rand(), rand(), rand(), rand()};
     int valsToBeSorted[] = {vals[0], vals[1], vals[2], vals[3], vals[4]};
 
     // Sort the array in place.
     int i, j;
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 5; i++)
+    {
         int aTemp = valsToBeSorted[i];
-        for (j = i - 1; j >= 0; j--) {
+        for (j = i - 1; j >= 0; j--)
+        {
             if (valsToBeSorted[j] <= aTemp)
                 break;
             valsToBeSorted[j + 1] = valsToBeSorted[j];
         }
-        valsToBeSorted [j + 1] = aTemp;
+        valsToBeSorted[j + 1] = aTemp;
     }
 
     // Print out the array.
     printf("[");
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < 4; i++)
+    {
         printf("%d, ", valsToBeSorted[i]);
     }
     printf("%d]\r\n", valsToBeSorted[i]);
@@ -61,7 +64,8 @@ int main(void) {
      * Returning from main() is bad form in embedded environments. So we
      * sit and spin.
      */
-    while(1<2);
+    while (1 < 2)
+        ;
     BOARD_End();
     return 0;
 }
